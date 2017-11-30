e074e7d6215db652cd7ef51b78ecc403b5e67e4f
ewnf <958102068@qq.com>
2017-11-30 11:57:17 +0800

    新增了背英语单词的功能\n新增了删除、查询功能\n

--- datebase/math.py
+++ datebase/math.py
@@ -1,8 +1,8 @@
 # integar_formula = [
 
 integar_ret_formula11 = [
-    ("sin[x]","cos[x]"),
-    ("cos[x]","-sin[x]"),
+    ("sin[x]","-cos[x]"),
+    ("cos[x]","sin[x]"),
     ("tan[x]","-ln|cosx|"),
     ("csc[x]","ln|csc[x]-cot[x]|"),
     ("sec[x]","ln|sec[x]+tan[x]|"),
@@ -17,5 +17,15 @@ integar_ret_formula01 = [
     ("-1/(a^2+x^2)","(1/a)arccot[x/a]"),
     ("1/√(1-x^2)","arcsin[x]"),
     ("-1/√(1-x^2)","arccos[x]")
+]
+
+'''
+和差化积系列
+'''
+sum_mil = [
+    ("sin[a]cos[b]","(1/2)(sin[a+b]+sin[a-b])"),
+    ("sin[a]sin[b]","-(1/2)(cos[a+b]-cos[a-b])"),
+    ("cos[a]cos[b]","(1/2)(cos[a+b]+cos[a-b])"),
+    ("cos[a]sin[b]","(1/2)(sin(a+b]-sin[a-b])")
 ]
 all = [integar_ret_formula01,integar_ret_formula11]
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 #!
+import datebase.english as en
 import datebase.math as mt
 import random
 tj=[0,0,0,0,0,0,0,0,0]
+last_one = None
 while(1):
-    t1 = random.randint(0,8)
-    t2 = random.randint(0,8)
-    t3 = random.random()
-    tj[int(t2)]=tj[int(t2)]+1
-    # print(t)
-    input(mt.integar_ret_formula11[int(t2)][0])
-    print("\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t"%(tj[0],tj[1],tj[2],tj[3],tj[4],tj[5],tj[6],tj[7],tj[8]),mt.integar_ret_formula11[int(t2)][1])
-    # print("\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d"%(tj[0],tj[1],tj[2],tj[3],tj[4],tj[5],tj[6],tj[7],tj[8]))
+    content = en.words_in_u4
+    len_1 = len(content)-1
+    t = random.randint(0,len_1)
+    content_in = input(content[t][0])
+    print(content[t][1])
+    if content_in =='y':
+        if last_one != None:
+            print("删除：%s\t%s"%(content[last_one][0],content[last_one][1]))
+            content.pop(last_one)
+            if last_one < t:
+                t = t - 1
+        else:print("这是第一次，别想着删上次的\n")
+    elif content_in == 't':
+        for i in content:
+            print(i[0],"  \t",i[1])
+        print("共计%d个内容"%len(content))
+    last_one = t
